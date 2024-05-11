8e1ac74396b691e896974ece0a589189e76a5009
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-11 08:20:51 +0800

    added mongodb connection

--- pages/api/foo.ts
+++ pages/api/foo.ts
@@ -1,5 +1,31 @@
-import { users } from '../../data'
+import { MongoClient } from 'mongodb';
 
-export default function handler(req, res) {
-  res.status(200).json(users)
+export default async function handler(req, res) {
+  const uri = '"mongodb+srv://Ronchiko:Mybabe0814@atlascluster.rjfmjfq.mongodb.net/my_cart_database?retryWrites=true&w=majority";' 
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    const db = client.db('my_cart_database.users');
+    const collection = db.collection('users');
+
+    if (req.method === 'GET') {
+      const users = await collection.find({}).toArray();
+      res.status(200).json(users);
+    } else if (req.method === 'POST') {
+      const newPost = req.body;
+      const result = await collection.insertOne(newPost);
+      res.status(201).json(result.ops[0]);
+    }
+
+
+    // Handle other CRUD operations (PUT, DELETE) similarly
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
+  } finally {
+    await client.close();
+  }
 }
