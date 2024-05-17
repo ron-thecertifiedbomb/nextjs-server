21ac5d9ed8b6a4e6ab29fffe45e1a7b589507385
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-17 22:53:46 +0800

    adsd

--- pages/api/users/authenticate/route.ts
+++ pages/api/users/authenticate/route.ts
@@ -1,7 +1,7 @@
-import bcrypt from 'bcrypt';
+
 import { NextApiRequest, NextApiResponse } from 'next';
 import connectToDatabase from '../../../../dbConfig/dbConfig';
-import jwt from "jsonwebtoken"
+
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
   let client;
@@ -17,6 +17,10 @@ export default async function handler(request: NextApiRequest, response: NextApi
     } = request.body;
 
 
+    const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
     if (!username || !password) {
         return response.status(400).json({ error: "Username and password are required" });
       }
@@ -29,10 +33,10 @@ export default async function handler(request: NextApiRequest, response: NextApi
       return response.status(409).json({ error: "Username does not exist" });
     }
 
-    // const validPassword = await bcrypt.compare(password, existingUser.password);
-    // if (!validPassword) {
-    //   return response.status(400).json({ error: "Invalid password" });
-    // }
+    const validPassword = await bcrypt.compare(password, existingUser.password);
+    if (!validPassword) {
+      return response.status(400).json({ error: "Invalid password" });
+    }
 
     // const tokenData = {
     //   id: existingUser._id,
@@ -40,18 +44,14 @@ export default async function handler(request: NextApiRequest, response: NextApi
     //   email: existingUser.email
     // };
 
-    // // Create a token with expiration of 1 day
-    // const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" });
+    // Create a token with expiration of 1 day
+    const token = await jwt.sign({ userId: existingUser._id }, process.env.TOKEN_SECRET!, { expiresIn: "1h" });
 
     // // Set the token as an HTTP-only cookie
     // response.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/; Max-Age=86400`);
 
     // Create a JSON response indicating successful login
-    return response.json({
-      message: "Login successful",
-      success: true,
-    });
-
+    response.status(200).json({ message: 'Authentication successful', userId: existingUser._id, token });
   } catch (error: any) {
     console.error('Error during login:', error);
     response.status(500).json({ message: 'Internal Server Error' });
