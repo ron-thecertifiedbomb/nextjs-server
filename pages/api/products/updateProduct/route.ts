6daef9bec17e329d1648743ef78ee7f3ed47f4c7
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-22 10:14:11 +0800

    test component

--- pages/api/products/updateProduct/route.ts
+++ pages/api/products/updateProduct/route.ts
@@ -2,22 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import connectToDatabase from "../../../../dbConfig/dbConfig";
 import { ObjectId } from "mongodb";
 
-const mockProduct = {
-  name: 'Xbox 360',
-  brand: 'Microsoft',
-  price: 499,
-  quantity: 10,
-  category: 'gaming console',
-  specifications: {
-    processor: 'Ronan',
-    graphics: 'Ronan',
-    storage: '1TB NVMe SSD',
-    resolution: '4K',
-    maxFrameRate: '120fps',
-  },
-  includedItems: ['Ronan', 'HDMI cable', 'Power cord', 'User manual'],
-  availableColors: ['Black'],
-};
 
 export default async function handler(
   request: NextApiRequest,
@@ -30,30 +14,32 @@ export default async function handler(
     const db = client.db("storage");
     const collection = db.collection("products");
     const productId = request.query._id as string;
+    const updateData = JSON.parse(request.body);
 
-    const existingProduct = await collection.findOne({ _id: new ObjectId(productId) });
+    const existingProduct = await collection.findOne({
+      _id: new ObjectId(productId),
+    });
 
-  
     if (!existingProduct) {
       response.status(404).json({ message: "Product not found" });
       return;
     }
 
-   
     const updatedProduct = await collection.findOneAndUpdate(
       { _id: new ObjectId(productId) },
-      { $set: mockProduct },
+      { $set: updateData },
       { returnDocument: "after" }
     );
 
-  
     response.status(200).json({
       message: "Product updated successfully",
       updatedProduct: updatedProduct.value,
     });
   } catch (error) {
     console.error("Error updating product:", error);
-    response.status(500).json({ message: "Error updating product", error: error.message });
+    response
+      .status(500)
+      .json({ message: "Error updating product", error: error.message });
   } finally {
     if (client) {
       await client.close();
