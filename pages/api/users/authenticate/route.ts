9cb3cf6fc20a41195a54290f07653965eabb4b07
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-27 18:04:02 +0800

    fixed

--- pages/api/users/authenticate/route.ts
+++ pages/api/users/authenticate/route.ts
@@ -42,6 +42,26 @@ export default async function handler(
     };
 
 
+
+    const getCurrentTime = (): number => {
+      return new Date().getTime();
+    };
+
+    const currentTime = getCurrentTime();
+
+    const result = await collection.findOneAndUpdate(
+      {
+        _id: existingUser._id
+      },
+      { $set: { lastLoggedIn: currentTime } },
+      { returnDocument: "after" }
+    );
+
+
+       if (!result.value) {
+        throw new Error("Failed to update lastLoggedIn");
+      }
+
     const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, {
       expiresIn: "1h",
     });
