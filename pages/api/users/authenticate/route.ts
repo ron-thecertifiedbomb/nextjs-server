70f36c7153c0f662f718d3a2a0db68d0aeef63cb
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-27 17:57:43 +0800

    removed time

--- pages/api/users/authenticate/route.ts
+++ pages/api/users/authenticate/route.ts
@@ -41,24 +41,24 @@ export default async function handler(
       email: existingUser.email,
     };
 
-    const getCurrentTime = (): number => {
-      return new Date().getTime();
-    };
+    // const getCurrentTime = (): number => {
+    //   return new Date().getTime();
+    // };
 
-    const currentTime = getCurrentTime();
+    // const currentTime = getCurrentTime();
 
-    const result = await collection.findOneAndUpdate(
-      {
-        _id: existingUser._id
-      },
-      { $set: { lastLoggedIn: currentTime } },
-      { returnDocument: "after" }
-    );
+    // const result = await collection.findOneAndUpdate(
+    //   {
+    //     _id: existingUser._id
+    //   },
+    //   { $set: { lastLoggedIn: currentTime } },
+    //   { returnDocument: "after" }
+    // );
 
 
-    if (!result.value) {
-      throw new Error("Failed to update lastLoggedIn");
-    }
+    // if (!result.value) {
+    //   throw new Error("Failed to update lastLoggedIn");
+    // }
 
 
     const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, {
