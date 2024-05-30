335cdcb371d9e4f28398e70a622c3616150240c4
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-05-30 21:55:41 +0800

    addasd

--- pages/api/products/cart/addtocart/route.ts
+++ pages/api/products/cart/addtocart/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import connectToDatabase from "../../../../../dbConfig/dbConfig";
-
+import { ObjectId } from "mongodb";
 
 export default async function POST(
   request: NextApiRequest,
@@ -12,46 +12,32 @@ export default async function POST(
     client = await connectToDatabase();
     const db = client.db("storage");
 
-    const {
-       data,
-    } = JSON.parse(request.body);
-
+    const { data } = JSON.parse(request.body);
 
     const collection = db.collection("cart");
 
-    const ownerId = await collection.findOne({ ownerId: data.ownerId });
+    const ownerIdObject = new ObjectId(data.ownerId);
+    
+    const owner = await collection.findOne({ ownerId: ownerIdObject });
 
-    response.status(200).json({
-      message: "ownerId" , ownerId,
-  
-    });
-
-
-    if (ownerId) {
+    if (owner) {
       await collection.updateOne(
-        { ownerId:  ownerId   },
+        { ownerId: ownerIdObject },
         {
           $push: {
-            CartItems: [
-              data
-            ]
-              ,
-          },
+            CartItems: data
+          }
         }
       );
-
 
       response.status(200).json({
         message: "Cart item successfully added to user's cart",
         CartItems: [data],
       });
-
     } else {
-      response.status(404).json({ message: "Failed to add item/s to cart" , data});
-    
+      response.status(404).json({ message: "Owner ID not found. Failed to add item to cart", data });
     }
   } catch (error) {
-    
     console.error("Error adding cart item to user's cart:", error);
     response.status(500).json({
       message: "Error adding cart item to user's cart",
