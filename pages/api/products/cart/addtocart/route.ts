beb92416c043d0829119924dd69d091779616415
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2024-06-13 16:01:16 +0800

    dasdsd

--- pages/api/products/cart/addtocart/route.ts
+++ pages/api/products/cart/addtocart/route.ts
@@ -2,16 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import connectToDatabase from "../../../../../dbConfig/dbConfig";
 import { ObjectId } from "mongodb";
 
-interface CartItem {
-  orderId: string;
-  productId: string;
-  name: string;
-  price: number;
-  quantityOrdered: number;
-  dateAdded: string;
-  timeAdded: string;
-}
-
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
@@ -21,9 +11,7 @@ export default async function handler(
   try {
     client = await connectToDatabase();
     const db = client.db('storage');
-    
-    // Check if request body exists and parse JSON
-    const body = request.body ? JSON.parse(request.body) : {};
+    const body = JSON.parse(request.body);
     console.log("Request body: ", body);
 
     const { ownerId, orderId, productId, name, price, quantityOrdered, dateAdded, timeAdded } = body;
@@ -33,32 +21,21 @@ export default async function handler(
       return response.status(400).json({ message: "Required fields are missing in the request body" });
     }
 
-    const cartItem: CartItem = {
-      orderId,
-      productId,
-      name,
-      price,
-      quantityOrdered,
-      dateAdded,
-      timeAdded
-    };
-
     const collection = db.collection('cart');
 
     // Check if the product already exists in the cart
     const existingCartItem = await collection.findOne({ ownerId: new ObjectId(ownerId), "cartItems.productId": productId });
 
     if (existingCartItem) {
-      // If the product exists, update its quantity and total price
+      // If the product exists, update its quantity and price
       await collection.updateOne(
         { ownerId: new ObjectId(ownerId), "cartItems.productId": productId },
         {
           $inc: {
             "cartItems.$.quantityOrdered": quantityOrdered,
-            "cartItems.$.price": price,
           },
           $set: {
-            "cartItems.$.price": price, // Update price in case it changed
+            "cartItems.$.price": price, // Update price
             "cartItems.$.dateAdded": dateAdded, // Update date added
             "cartItems.$.timeAdded": timeAdded, // Update time added
           }
@@ -66,6 +43,16 @@ export default async function handler(
       );
     } else {
       // If the product doesn't exist, push it into the cartItems array
+      const cartItem = {
+        orderId,
+        productId,
+        name,
+        price,
+        quantityOrdered,
+        dateAdded,
+        timeAdded
+      };
+
       await collection.updateOne(
         { ownerId: new ObjectId(ownerId) },
         {
