c49b526c127378bebb7bde4401a29f921b874613
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-02-12 16:55:10 +0800

    removed cors

--- pages/api/food/getAllFoodList/route.ts
+++ pages/api/food/getAllFoodList/route.ts
@@ -3,9 +3,9 @@ import connectToDatabase from '../../../../dbConfig/dbConfig';
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
