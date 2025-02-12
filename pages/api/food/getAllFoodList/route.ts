85f4ee6c6493af818d4807eaee30d955107fb5c5
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-02-12 13:44:05 +0800

    disabled cors

--- pages/api/food/getAllFoodList/route.ts
+++ pages/api/food/getAllFoodList/route.ts
@@ -2,24 +2,30 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import connectToDatabase from '../../../../dbConfig/dbConfig';
 
 
-export default async function GET(request: NextApiRequest, response: NextApiResponse) {
-  let client;
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  let client;
   try {
     client = await connectToDatabase();
-    const db = client.db('storage');
-    const collection = db.collection('foodstorage');
+    const db = client.db("storage");
+    const collection = db.collection("foodstorage");
 
-    if (request.method === 'GET') {
+    if (req.method === "GET") {
       const foodstorage = await collection.find({}).toArray();
-      response.status(200).json(foodstorage);
-      console.log(foodstorage)
+      return res.status(200).json(foodstorage);
     }
 
   } catch (err) {
-    console.log(err.stack);
-    response.status(500).json({ message: 'Internal Server Error' });
+    console.error(err);
+    return res.status(500).json({ message: "Internal Server Error" });
   } finally {
-    await client.close();
+    if (client) await client.close();
   }
 }
