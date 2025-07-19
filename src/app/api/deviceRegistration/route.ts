60e7762040ccb9be292df8e195e167358b670acf
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-07-20 07:10:19 +0800

    added basic auth

--- src/app/api/deviceRegistration/route.ts
+++ src/app/api/deviceRegistration/route.ts
@@ -1,6 +1,21 @@
-// /app/api/deviceRegistration/route.ts
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+
+const basicAuth = (req: NextRequest) => {
+  const authHeader = req.headers.get("authorization");
+  if (!authHeader || !authHeader.startsWith("Basic ")) return false;
+
+  const base64Credentials = authHeader.split(" ")[1];
+  const credentials = Buffer.from(base64Credentials, "base64").toString(
+    "utf-8"
+  );
+  const [username, password] = credentials.split(":");
+
+  const validUsername = process.env.BASIC_AUTH_USERNAME;
+  const validPassword = process.env.BASIC_AUTH_PASSWORD;
+
+  return username === validUsername && password === validPassword;
+};
 
 export async function OPTIONS() {
   return new NextResponse(null, {
@@ -8,12 +23,16 @@ export async function OPTIONS() {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
     },
   });
 }
 
 export async function POST(req: NextRequest) {
+  if (!basicAuth(req)) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
   const body = await req.json();
   const {
     tokenCode,
@@ -28,12 +47,7 @@ export async function POST(req: NextRequest) {
   if (!tokenCode || !phone || !dateRegistered || !user_id) {
     return NextResponse.json(
       { message: "Missing required fields" },
-      {
-        status: 400,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
+      { status: 400 }
     );
   }
 
@@ -66,35 +80,21 @@ export async function POST(req: NextRequest) {
     console.log(`✅ FCM details sent via email.`);
     return NextResponse.json(
       { message: "FCM details emailed successfully" },
-      {
-        status: 200,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
+      { status: 200 }
     );
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error("❌ Failed to send email:", error);
       return NextResponse.json(
         { message: "Failed to send email", error: error.message },
-        {
-          status: 500,
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-          },
-        }
+        { status: 500 }
       );
     }
 
+    console.error("❌ Unknown error occurred", error);
     return NextResponse.json(
       { message: "Failed to send email", error: "Unknown error" },
-      {
-        status: 500,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
+      { status: 500 }
     );
   }
 }
